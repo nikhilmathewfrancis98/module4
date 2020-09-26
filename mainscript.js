d04877331966248e303ea21a6521c5effa6fa044
nikhilmathewfrancis98 <nikhilmathewfrancis@gmail.com>
2020-09-26 10:00:18 +0530

    Module 4 Project

--- mainscript.js
+++ mainscript.js
@@ -1,9 +1,9 @@
 (function () {
 
-var names = ["Nikhil", "Jayson", "Jeorge", "Jimbru", "Paul", "Mathew", "Francis", "Janaki", "Jolly", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-
+ 
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
